0db755710c8f497f7c548e2e87c10fbacac6f5ba
rafa <monteir20@icloud.com>
2026-06-10 10:08:55 -0100

    apollo providers

--- servidor-local-front/app/layout.tsx
+++ servidor-local-front/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
+import { Providers } from "./providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,8 +32,10 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+         <Providers>
         <Toaster position="top-right" richColors expand />
         {children}
+         </Providers>
       </body>
     </html>
   );
